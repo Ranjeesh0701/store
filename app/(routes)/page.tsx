11278c6b8e0f188fca3b33e7eadae3f2a5e04db1
Ranjeesh0701 <techstack0701@gmail.com>
2023-08-22 22:32:49 +0530

    add store

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
+import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
+import Container from "@/components/ui/Container";
+
+export const revalidate = 0;
+
+const Page = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("3c96eb2d-9dd6-441a-9c9d-d1dfbb6acbde");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Page;
---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import ModalProvider from "@/providers/ModalProvider";
+import ToastProvider from "@/providers/ToastProvider";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>
+        <ModalProvider />
+        <ToastProvider />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
